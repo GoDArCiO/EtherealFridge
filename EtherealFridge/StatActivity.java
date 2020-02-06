a3cbaedbea284acb882b5491f3d8be176185b704
GoDArCiO <46372307+GoDArCiO@users.noreply.github.com>
2020-02-06 04:33:11 +0100

    Add files via upload

--- EtherealFridge/StatActivity.java
+++ EtherealFridge/StatActivity.java
@@ -1,10 +1,13 @@
 package com.example.EtherealFridge;
 
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +23,22 @@ public class StatActivity extends AppCompatActivity {
         private Button Reset,Set;
         private Integer kcal=0,bialko=0,tluszcz=0;
         private Integer Zkcal=0,Zbialko=0,Ztluszcz=0,Zkcal3=0,Zbialko3=0,Ztluszcz3=0;
-        private EditText editText1,editText2,editText3;
+        private EditText editText1,editText2,editText3,weight;
+        private Switch sex,body,sport;
+        private CheckBox rem;
+
+    private String text,text1,text2,text3;
+    private boolean switchOnOff1,switchOnOff2,switchOnOff3;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String TEXTc = "textc";
+    public static final String TEXTp = "textp";
+    public static final String TEXTf = "textf";
+    public static final String SWITCH1 = "switch1";
+    public static final String SWITCH2 = "switch2";
+    public static final String SWITCH3 = "switch3";
+
         DatabaseHelper mDatabaseHelper;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +61,20 @@ public class StatActivity extends AppCompatActivity {
             editText1 = (EditText) findViewById(R.id.editText6);
             editText2 = (EditText) findViewById(R.id.editText7);
             editText3 = (EditText) findViewById(R.id.editText8);
-
+            weight = (EditText) findViewById(R.id.editText9);
+            sex = (Switch) findViewById(R.id.sex);
+            body = (Switch) findViewById(R.id.body);
+            sport = (Switch) findViewById(R.id.sport);
+            rem = (CheckBox) findViewById(R.id.remember);
 
             mDatabaseHelper = new DatabaseHelper(this);
+
+            loadData();
+            updateViews();
+            String weights = weight.getText().toString();
+            String kcals = editText1.getText().toString();
+            String proteins = editText2.getText().toString();
+            String fats = editText3.getText().toString();
 
             Reset.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -63,30 +92,134 @@ public class StatActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     int kcalA,proteinA,fatA;
+                    double WeightA;
             String kcals = editText1.getText().toString();
             String proteins = editText2.getText().toString();
             String fats = editText3.getText().toString();
-            //walidacja czy podany text jest zlozony z cyfr
-                    if(kcals.matches("-?\\d+")&&proteins .matches("-?\\d+")&&fats.matches("-?\\d+")) {
-                        kcalA = Integer.parseInt(kcals);
-                        fatA = Integer.parseInt(fats);
-                        proteinA = Integer.parseInt(proteins);
+            String weights = weight.getText().toString();
+                    //automat pelny
+                    if(!weights.isEmpty()){
+                        //oba pelne
+                        if(!kcals.isEmpty()||!proteins.isEmpty()||!fats.isEmpty()){
+                            toastMessage("Cant calculate manually and automatically both!");
+                            return;
+                        }else{
+                            //calculate cal
+                            WeightA = Double.parseDouble(weights);
+                            if(sex.isChecked()){
+                                WeightA*=0.9;
+                            }
+                            WeightA*=24;
+                            if(body.isChecked()){
+                                WeightA*=0.9;
+                            }else{
+                                WeightA*=0.95;
+                            }
+                            if(sport.isChecked()){
+                                WeightA*=1.80;
+                            }else{
+                                WeightA*=1.55;
+                            }
+                            kcalA= (int) WeightA;
+
+                            //calculate protein
+                            WeightA = Double.parseDouble(weights);
+                            if(sex.isChecked()){
+                                WeightA*=0.9;
+                            }else{
+                                WeightA*=1.2;
+                            }
+                            if(sport.isChecked()){
+                                WeightA*=1.05;
+                            }else{
+                                WeightA*=0.95;
+                            }
+                            proteinA= (int) WeightA;
+
+                            //calculate fat
+                            WeightA=kcalA*0.25;
+                            WeightA/=9;
+                            fatA= (int) WeightA;
+                        }
                     }else{
-                        toastMessage("Your input needs to be Integer!");
-                        return;
+                        //oba puste
+                        if(kcals.isEmpty()||proteins.isEmpty()||fats.isEmpty()){
+                            toastMessage("Please enter weight or needs!");
+                            return;
+                        }else{//manual
+                            //walidacja czy podany text jest zlozony z cyfr
+                            if(kcals.matches("-?\\d+")&&proteins .matches("-?\\d+")&&fats.matches("-?\\d+")) {
+                                kcalA = Integer.parseInt(kcals);
+                                fatA = Integer.parseInt(fats);
+                                proteinA = Integer.parseInt(proteins);
+                            }else{
+                                toastMessage("Your input needs to be Integer!");
+                                return;
+                            }
+                        }
                     }
+
                     calp.setText(""+Zkcal*100/kcalA+"%");
                     prop.setText(""+Zbialko*100/proteinA+"%");
                     fatp.setText(""+Ztluszcz*100/fatA+"%");
                     cal3p.setText(""+Zkcal3*100/(kcalA*30)+"%");
                     pro3p.setText(""+Zbialko3*100/(proteinA*30)+"%");
                     fat3p.setText(""+Ztluszcz3*100/(fatA*30)+"%");
+
+                    if(rem.isChecked()){
+                        saveData();
+                    }
+
                 }
             });
 
             setStats();
 
+            if(!weights.isEmpty()){
+                Set.performClick();
+            }
+            if(!kcals.isEmpty()||!proteins.isEmpty()||!fats.isEmpty()) {
+                Set.performClick();
+            }
+
     }
+
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, weight.getText().toString());
+        editor.putString(TEXTc, editText1.getText().toString());
+        editor.putString(TEXTp, editText2.getText().toString());
+        editor.putString(TEXTf, editText3.getText().toString());
+        editor.putBoolean(SWITCH1, sex.isChecked());
+        editor.putBoolean(SWITCH2, body.isChecked());
+        editor.putBoolean(SWITCH3, sport.isChecked());
+
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        text1 = sharedPreferences.getString(TEXTc, "");
+        text2 = sharedPreferences.getString(TEXTp, "");
+        text3 = sharedPreferences.getString(TEXTf, "");
+        switchOnOff1 = sharedPreferences.getBoolean(SWITCH1, false);
+        switchOnOff2 = sharedPreferences.getBoolean(SWITCH2, false);
+        switchOnOff3 = sharedPreferences.getBoolean(SWITCH3, false);
+    }
+
+    public void updateViews() {
+        weight.setText(text);
+        editText1.setText(text1);
+        editText2.setText(text2);
+        editText3.setText(text3);
+        sex.setChecked(switchOnOff1);
+        body.setChecked(switchOnOff2);
+        sport.setChecked(switchOnOff3);
+    }
+
         private void setStats() {
 
             Calendar calendar= Calendar.getInstance();
